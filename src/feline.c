7334d5427fb00970d66e23a8f85de3e7ce8f7947
james <67040221+iJimmy500@users.noreply.github.com>
2026-05-17 15:41:51 -0500

    chore: bump version to 1.1.0

--- src/feline.c
+++ src/feline.c
@@ -72,7 +72,7 @@ int main(int argc, char *argv[]) {
     }
 
     if (strcmp(argv[1], "--version") == 0 || strcmp(argv[1], "-v") == 0) {
-        printf("feline 1.0.0\n");
+        printf("feline 1.1.0\n");
         return 0;
     }
 
